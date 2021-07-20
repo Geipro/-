d7c5b328ff8dcc9bf5387b4a8b7900f3b9f69acb
양동혁 <lukigreen@naver.com>
2021-07-20 14:50:57 +0900

    feat : JPA 기능 추가 Room Search(Room_ID) 기능 추가

--- backend/Room/room/src/main/java/com/ssafy/room/dto/Room.java
+++ backend/Room/room/src/main/java/com/ssafy/room/dto/Room.java
@@ -1,14 +1,47 @@
 package com.ssafy.room.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Table(name="Room")
+@Entity
 public class Room {
-	private int room_id;
-	private int game_id;
+	// GenerationType.IDENTITY를 통해서 AutoIncrease가 가능 -> 기본키 생성을 데이터베이스에 위임한다
+	// ID - 기본키
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int roomid;
+	
+	// @Column -> 데이터베이스 이름과 Class이름이 다르면 사용하기
+	
+	private int gameid;
 	private String name;
 	private String password;
 	private boolean playing;
+	
+	public int getRoomid() {
+		return roomid;
+	}
+	public int getGameid() {
+		return gameid;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public boolean isPlaying() {
+		return playing;
+	}
+	
+	
+	
+	
 }
--- backend/Room/room/src/main/java/com/ssafy/room/service/RoomServiceImpl.java
+++ backend/Room/room/src/main/java/com/ssafy/room/service/RoomServiceImpl.java
@@ -5,44 +5,49 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.room.dao.RoomDao;
 import com.ssafy.room.dto.PageBean;
 import com.ssafy.room.dto.Room;
+import com.ssafy.room.repository.RoomRespository;
 
 @Service
 public class RoomServiceImpl implements RoomService {
-
+	
 	@Autowired
-	private RoomDao dao;
+	private RoomRespository roomRespository;
 
 	@Override
 	public List<Room> searchAll(PageBean bean) {
 		// 한번에 6개 출력
 		bean.setInterval(6);
-		int total = dao.totalCount();
-		
-		return dao.searchAll(bean);
+		return null;
 	}
 
 	@Override
 	public Room searchRoom(int room_id) {
-		return dao.searchRoom(room_id);
+		return roomRespository.findRoomByRoomid(room_id);
 	}
 
 	@Override
 	public Room insertRoom(Room room) {
-		return dao.insertRoom(room);
+		return null;
 	}
 
 	@Override
 	public Room updateRoom(Room room) {
-		return dao.updateRoom(room);
+		return null;
 	}
 
 	@Override
 	public boolean deleteRoom(int room_id) {
-		return dao.deleteRoom(room_id);
+		return false;
 	}
 	
-	
+	public static void main(String[] args) {
+		System.out.println("A");
+		RoomService roomService = new RoomServiceImpl();
+		System.out.println("B");
+		Room room = roomService.searchRoom(1);
+		System.out.println("C");
+		
+	}
 }
